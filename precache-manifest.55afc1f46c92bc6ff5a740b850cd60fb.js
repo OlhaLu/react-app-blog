f79b755383a4dc7b6f543335bc92b0d626ddd808
OlhaLu <olhalutsyshyna@gmail.com>
2020-03-28 21:08:20 +0200

    Updates

---
+++ precache-manifest.55afc1f46c92bc6ff5a740b850cd60fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53330ba61b67cd349c49fdb73b2eb77b",
+    "url": "/react-app-blog/index.html"
+  },
+  {
+    "revision": "7de26df9a1b60ea177a2",
+    "url": "/react-app-blog/static/css/main.a470a40e.chunk.css"
+  },
+  {
+    "revision": "30f479f324938d34ee30",
+    "url": "/react-app-blog/static/js/2.c7e938ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app-blog/static/js/2.c7e938ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de26df9a1b60ea177a2",
+    "url": "/react-app-blog/static/js/main.f06d0168.chunk.js"
+  },
+  {
+    "revision": "3525f19f89fadbbac4ef",
+    "url": "/react-app-blog/static/js/runtime-main.0dc86fe5.js"
+  }
+]);
